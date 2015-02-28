66da74ff91576531616fd6e551be6bc10477eadc
Dave Wasmer <davewasmer@gmail.com>
2015-02-28 13:23:24 -0500

    Switch to wrapping ember-cli-coffeescript
    This seems like a cleaner overall approach. Rather than relying on Ember-CLI to order the execution of these filters, we can just leverage Broccoli itself, since this is exactly what it is designed to do.
    
    With this commit, ember-cli-coffees6 now directly imports ember-cli-coffeescript, and invokes it internally on the output of the ES6 filter.

--- index.js
+++ index.js
@@ -2,58 +2,73 @@
 
 var Filter = require('broccoli-filter')
 var checker = require('ember-cli-version-checker');
+var debug = require('debug')('ember-cli:ember-cli-coffees6');
+var CoffeescriptAddon = require('ember-cli-coffeescript');
+var coffeescriptOptions = CoffeescriptAddon.getConfig;
+var CoffeescriptPreprocessor = require('ember-cli-coffeescript/lib/coffee-preprocessor');
 
-CoffeeScriptEs6Filter.prototype = Object.create(Filter.prototype);
-CoffeeScriptEs6Filter.prototype.constructor = CoffeeScriptEs6Filter
+//
+// Broccoli Filter
+//
 
-function CoffeeScriptEs6Filter (inputTree, options) {
-
-  if (!(this instanceof CoffeeScriptEs6Filter)) return new CoffeeScriptEs6Filter(inputTree, options);
+function CoffeescriptEs6Filter (inputTree, options) {
+  if (!(this instanceof CoffeescriptEs6Filter)) return new CoffeescriptEs6Filter(inputTree, options);
   this.options = options;
   Filter.call(this, inputTree)
 }
 
-CoffeeScriptEs6Filter.prototype.extensions = ['coffee']
-CoffeeScriptEs6Filter.prototype.targetExtension = 'coffee'
+CoffeescriptEs6Filter.prototype = Object.create(Filter.prototype);
+CoffeescriptEs6Filter.prototype.constructor = CoffeescriptEs6Filter;
 
+CoffeescriptEs6Filter.prototype.extensions = CoffeescriptPreprocessor.prototype.ext;
+CoffeescriptEs6Filter.prototype.targetExtension = 'coffee'
 
+CoffeescriptEs6Filter.prototype.processString = function (string, src) {
+  debug("Coffees6 Input:\n\n", string, "\n=======");
 
-CoffeeScriptEs6Filter.prototype.processString = function (string) {
-  if(this.options.debug) {
-    console.log("Coffees6 Input:\n\n", string, "\n=======");
-  }
-  var regex = /^export default /m;
-  if (string.match(regex)) {
-    string = string.replace(regex, "___ExportObject___ = ");
+  // Fix export statements
+  var exportRegex = /^export default /m;
+  if (string.match(exportRegex)) {
+    string = string.replace(exportRegex, "___ExportObject___ = ");
     string = string + "\n\n`export default ___ExportObject___`";
   }
 
+  // Fix import statements
   var importRegex = /^import (.+) from ("|')(.+)("|')( \t)*$/gm
   var match;
-
   while (match = importRegex.exec(string)) {
     string = string.replace(match[0], "`" + match[0] + ";`");
   }
-  if(this.options.debug) {
-    console.log("Coffees6 Output:\n\n", string, "\n=======");
-  }
+  debug("Coffees6 Output:\n\n", string, "\n=======");
 
   return string;
 }
 
-function CoffeeES6Preprocessor(options) {
+
+//
+// Ember-CLI Preprocessor Plugin
+//
+
+function CoffeeES6Preprocessor(project, options) {
+  this.project = project;
   this.options = options || {};
   this.name = 'ember-cli-coffees6';
-  this.ext = 'js';
 }
 
+CoffeeES6Preprocessor.prototype.ext = CoffeescriptEs6Filter.prototype.extensions;
 CoffeeES6Preprocessor.prototype.toTree = function(tree, inputPath, outputPath) {
-  return new CoffeeScriptEs6Filter(tree, this.options);
+  var es6ified = new CoffeescriptEs6Filter(tree, this.options);
+  return new CoffeescriptPreprocessor(coffeescriptOptions.call(this)).toTree(es6ified);
 };
 
+
+//
+// Ember-CLI Addon
+//
+
 function CoffeescriptES6Addon(project) {
-  this._project = project;
-  this.name     = 'Ember CLI Coffeescript ES6 Addon';
+  this.project = project;
+  this.name = 'Ember CLI Coffeescript ES6 Addon';
 }
 
 CoffeescriptES6Addon.prototype.shouldSetupRegistryInIncluded = function() {
@@ -61,7 +76,7 @@ CoffeescriptES6Addon.prototype.shouldSetupRegistryInIncluded = function() {
 }
 
 CoffeescriptES6Addon.prototype.setupPreprocessorRegistry = function(type, registry) {
-  var plugin = new CoffeeES6Preprocessor(this._project.config(process.env.EMBER_ENV).coffeeES6);
+  var plugin = new CoffeeES6Preprocessor(this.project, {/* config here */});
   registry.add('js', plugin);
 }
 
